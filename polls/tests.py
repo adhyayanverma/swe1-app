9855c572a953ba24c0831cd97d6fc573ed00436f
av4159 <av4159@nyu.edu>
2026-03-06 18:05:52 -0500

    travis

--- polls/tests.py
+++ polls/tests.py
@@ -3,6 +3,7 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse, resolve
 
 
 class QuestionModelTests(TestCase):
@@ -23,3 +24,36 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
+
+class ProjectURLsTests(TestCase):
+    def test_admin_url_resolves(self):
+        """Checks if the admin URL is correctly configured."""
+        url = reverse('admin:index')
+        self.assertEqual(resolve(url).func.__name__, 'index')
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = Question.objects.create(
+            question_text="Future question.",
+            pub_date=timezone.now() + datetime.timedelta(days=5)
+        )
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = Question.objects.create(
+            question_text="Past Question.",
+            pub_date=timezone.now() - datetime.timedelta(days=5)
+        )
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
