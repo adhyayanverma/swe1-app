33e592276cddba566f2bfc5dca10c6fff91df061
av4159 <av4159@nyu.edu>
2026-03-06 18:29:38 -0500

    travis

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
 from .models import Choice, Question
 
 
@@ -27,6 +26,7 @@ class DetailView(generic.DetailView):
     def get_queryset(self):
         """Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
