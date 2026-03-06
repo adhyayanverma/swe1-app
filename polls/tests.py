7e18ab85947729635ce9c736a17b74895eee864b
av4159 <av4159@nyu.edu>
2026-03-06 17:29:38 -0500

    travis

--- polls/tests.py
+++ polls/tests.py
@@ -1,2 +1,47 @@
-
 # Create your tests here.
+import datetime
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+from django.urls import reverse
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the index page.
+        """
+        Question.objects.create(question_text="Past question.", pub_date=timezone.now() - datetime.timedelta(days=30))
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            ["<Question: Past question.>"]
+        )
